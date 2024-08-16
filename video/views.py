769cb25e8915d28aa04762b7fa309586a1c450ec
Navaneethveer3 <navaneeth42.com@gmail.com>
2024-08-16 10:36:16 -0700

    views.py

--- video/views.py
+++ video/views.py
@@ -26,7 +26,7 @@ def get_video_qualities(request):
 
         try:
             ydl_opts = {
-                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
             }
             with yt_dlp.YoutubeDL() as ydl:
                 info = ydl.extract_info(link, download=False)
