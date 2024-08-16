ebbd16493761d89e79d8dd38e9596b3a6d1b2015
Navaneethveer3 <navaneeth42.com@gmail.com>
2024-08-16 07:47:44 -0700

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,4 +137,19 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     'https://channel-1.onrender.com',
 ]
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'x-csrftoken',
+    'authorization',
+    # Add other headers if needed
+]
 
