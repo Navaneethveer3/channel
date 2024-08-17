c6525b8ed5558790273c528985af29901b3f3f19
Navaneethveer3 <navaneeth42.com@gmail.com>
2024-08-17 08:50:33 +0530

    views.py

--- video/views.py
+++ video/views.py
@@ -14,7 +14,7 @@ def myproject(request):
     return render(request, 'myproject.html')
 
 def get_video_info(api_key, video_id):
-    url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={api_key}&part=contentDetails,snippet'
+    url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key=AIzaSyD_znizOfuO62ZLybi1vXfM-IyWgA8ymQ8&part=contentDetails,snippet'
     response = requests.get(url)
     data = response.json()
     
